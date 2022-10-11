cdab87c1479f31120d113700fb4755d289820bc5
revive <wa143825@outlook.com>
2022-10-11 14:48:11 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="index">
+    <view class="w-300rpx h-300rpx pl-30rpx ml-30rpx bg-gray  text-red">
+      <text>{{ msg }}</text>
+    </view>
+  </view>
+</template>
+
+<script>
+import { ref } from "vue";
+import "./index.scss";
+
+export default {
+  setup() {
+    const msg = ref("Hello world");
+    return {
+      msg
+    };
+  }
+};
+</script>
